a1a977b721558da25e0219ba5966b7f4941b615b
z <z.y.j.1379@gmail.com>
2021-07-22 14:09:49 +0430

    add index.html

--- SAD_PROJECT/SAD/views.py
+++ SAD_PROJECT/SAD/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+from django.template import loader
+
+
+def index(request):
+    template = loader.get_template('SAD/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
