86efe8304ae2eb14134e85708bf74e505176ce2a
z <z.y.j.1379@gmail.com>
2021-07-22 14:50:08 +0430

    add signup and login html

--- SAD_PROJECT/SAD/models.py
+++ SAD_PROJECT/SAD/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
 
     def __str__(self):
-        return str(self.first_name) + " " + str(self.last_name)
+        return str(self.username)
--- SAD_PROJECT/SAD/urls.py
+++ SAD_PROJECT/SAD/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login_page, name='login_page'),
+    path('signup/', views.signup_page, name='signup_page'),
 ]
