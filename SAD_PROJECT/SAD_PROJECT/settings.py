7205be9147a19885c4d62707c8bb72ac22c13155
z <z.y.j.1379@gmail.com>
2021-07-24 22:48:52 +0430

    add errors

--- SAD_PROJECT/SAD_PROJECT/settings.py
+++ SAD_PROJECT/SAD_PROJECT/settings.py
@@ -12,7 +12,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from django.contrib.messages import constants as messages
 
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
