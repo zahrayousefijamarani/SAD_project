e7da93396b74cf602a1d5f80fe311a1742da6ab5
dahlia davoudi <45297749+dahlia-davoudi@users.noreply.github.com>
2021-08-23 02:33:20 +0430

    Update urls.py

--- SAD_PROJECT/SAD_PROJECT/urls.py
+++ SAD_PROJECT/SAD_PROJECT/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls'), name='login_page'),
-    path('', include('main.urls'))
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls"), name="login_page"),
+    path("", include("main.urls"))
 ]
