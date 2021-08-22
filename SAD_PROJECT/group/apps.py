ff4c883a0f4819a8067345ba6ab9e6a951190703
dahlia davoudi <45297749+dahlia-davoudi@users.noreply.github.com>
2021-08-23 03:35:56 +0430

    Update apps.py

--- SAD_PROJECT/group/apps.py
+++ SAD_PROJECT/group/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class GroupConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'group'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "group"
