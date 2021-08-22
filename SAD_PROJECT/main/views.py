5d8f86825ef53c33401809a45150cd5ecca122d8
dahlia davoudi <45297749+dahlia-davoudi@users.noreply.github.com>
2021-08-23 02:03:59 +0430

    Update views.py

--- SAD_PROJECT/main/views.py
+++ SAD_PROJECT/main/views.py
@@ -25,15 +25,19 @@ def register_request(request):
             return redirect("main:homepage")
         messages.error(request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
-    return render(request=request, template_name="main/register.html", context={"register_form": form})
+    return render(
+        request=request,
+        template_name="main/register.html",
+        context={"register_form": form},
+    )
 
 
 def login_request(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
@@ -44,7 +48,9 @@ def login_request(request):
         else:
             messages.error(request, "Invalid username or password.")
     form = AuthenticationForm()
-    return render(request=request, template_name="main/login.html", context={"login_form": form})
+    return render(
+        request=request, template_name="main/login.html", context={"login_form": form}
+    )
 
 
 def logout_request(request):
@@ -54,9 +60,9 @@ def logout_request(request):
 
 
 def contact_request(request):
-    template = loader.get_template('main/contacts.html')
+    template = loader.get_template("main/contacts.html")
     acc = Account.get_account_by_user(request.user.id)
-    context = {'contacts': Contact.get_contacts(acc)}
+    context = {"contacts": Contact.get_contacts(acc)}
     return HttpResponse(template.render(context, request))
 
 
@@ -64,161 +70,184 @@ def accept_contact(request, me, sec_id):
     me = Account.get_account_by_user(me)
     sec_user = Account.get_account_by_user(sec_id)
     Contact.add_contact(me, sec_user)
-    return HttpResponseRedirect(reverse('main:homepage', args=()))
+    return HttpResponseRedirect(reverse("main:homepage", args=()))
 
 
 def add_contact_request(request):  # send a form
     if request.method == "POST":
-        checked_id = request.POST.getlist('tag')
+        checked_id = request.POST.getlist("tag")
         for id in checked_id:
             me = Account.get_account_by_user(request.user.id)
             sec_user = Account.get_account_by_user(id)
-            print('click http://127.0.0.1:8000/accept_contact/' + str(request.user.id) + '/' + str(id))
+            print(
+                "click http://127.0.0.1:8000/accept_contact/"
+                + str(request.user.id)
+                + "/"
+                + str(id)
+            )
             print("----------------")
-            result = send_mail('Contact with ' + me.user.username,
-                               'click http://127.0.0.1:8000/accept_contact/' + str(request.user.id) + '/' + str(id),
-                               settings.EMAIL_HOST_USER,
-                               [sec_user.user.email]
-                               )
+            result = send_mail(
+                "Contact with " + me.user.username,
+                "click http://127.0.0.1:8000/accept_contact/"
+                + str(request.user.id)
+                + "/"
+                + str(id),
+                settings.EMAIL_HOST_USER,
+                [sec_user.user.email],
+            )
             print(result)
-        return HttpResponseRedirect(reverse('main:contacts', args=()))
+        return HttpResponseRedirect(reverse("main:contacts", args=()))
 
     else:
-        template = loader.get_template('main/add_contact.html')
+        template = loader.get_template("main/add_contact.html")
         all_account = Account.get_all_accounts()
         c = Contact.get_contacts(Account.get_account_by_user(request.user.id))
-        contacts = list([i['id'] for i in c])
+        contacts = list([i["id"] for i in c])
         accounts = []
         for acc in all_account:
-            if not (acc['id'] == request.user.id or acc['id'] in contacts):
+            if not (acc["id"] == request.user.id or acc["id"] in contacts):
                 accounts.append(acc)
 
-        context = {'users': accounts}
+        context = {"users": accounts}
         return HttpResponse(template.render(context, request))
 
 
 def group_request(request):
-    template = loader.get_template('main/group.html')
+    template = loader.get_template("main/group.html")
     acc = Account.get_account_by_user(request.user.id)
     context = {'groups': Group.get_all_group(acc)}
     return HttpResponse(template.render(context, request))
 
 
 def group_member_request(request, group_id):
-    template = loader.get_template('main/choose_member.html')
+    template = loader.get_template("main/choose_member.html")
     acc = Account.get_account_by_user(request.user.id)
-    context = {'contacts': Contact.get_contacts(acc),
-               'g_id': group_id}
+    context = {"contacts": Contact.get_contacts(acc), "g_id": group_id}
     return HttpResponse(template.render(context, request))
 
 
 def acc_group_request(request, group_id, user_id):
     Group.add_members(group_id, [user_id])
-    return HttpResponseRedirect(reverse('main:homepage', args=()))
+    return HttpResponseRedirect(reverse("main:homepage", args=()))
 
 
 def done_group_member_request(request, group_id):
-    checked_id = request.POST.getlist('tag')
+    checked_id = request.POST.getlist("tag")
     for id in checked_id:
         acc = Account.get_account_by_user(id)
         print(acc.user.email)
-        print('click http://127.0.0.1:8000/accept_gp/' + str(group_id) + '/' + str(acc.user.id))
+        print("click http://127.0.0.1:8000/accept_gp/"
+              + str(group_id)
+              + "/"
+              + str(acc.user.id)
+             )
         print("----------------")
-        result = send_mail('Inviting Group',
-                           'click http://127.0.0.1:8000/accept_gp/' + str(group_id) + '/' + str(acc.user.id),
-                           settings.EMAIL_HOST_USER,
-                           [acc.user.email]
-                           )
+        result = send_mail(
+            "Inviting Group",
+            "click http://127.0.0.1:8000/accept_gp/"
+            + str(group_id)
+            + "/"
+            + str(acc.user.id),
+            settings.EMAIL_HOST_USER,
+            [acc.user.email],
+        )
         print(result)
-    return HttpResponseRedirect(reverse('main:show_group', args=(group_id,)))
+    return HttpResponseRedirect(reverse("main:show_group", args=(group_id,)))
 
 
 def add_group_request(request):  # send a form
-    if request.method == 'POST':
+    if request.method == "POST":
         form = GroupForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data.get('group_name')
+            name = form.cleaned_data.get("group_name")
             pk = Group.create_group(name, Account.get_account_by_user(request.user.id))
-            return HttpResponseRedirect(reverse('main:group_member', args=(pk,)))
+            return HttpResponseRedirect(reverse("main:group_member", args=(pk,)))
         else:
             messages.error(request, "Invalid name")
     else:
         form = GroupForm()
-    return render(request, 'main/add_group.html', {'form': form})
+    return render(request, "main/add_group.html", {"form": form})
 
 
 def show_group_request(request, group_id):
-    template = loader.get_template('main/specific_group.html')
+    template = loader.get_template("main/specific_group.html")
     gp = Group.get_group(group_id)
-    context = {'members': gp.get_members(gp), 'group_id': group_id,
-               'shares': Share.get_shares_for_gp(group_id)}
+    context = {
+        "members": gp.get_members(gp),
+        "group_id": group_id,
+        "shares": Share.get_shares_for_gp(group_id)
+    }
     return HttpResponse(template.render(context, request))
 
 
 def homepage(request):
     if not request.user.is_authenticated:
-        template = loader.get_template('main/index.html')
+        template = loader.get_template("main/index.html")
         context = {}
         return HttpResponse(template.render(context, request))
     else:
-        template = loader.get_template('main/home.html')
+        template = loader.get_template("main/home.html")
         acc = Account.get_account_by_user(request.user.id)
         context = acc.serializer()
         return HttpResponse(template.render(context, request))
 
 
 def report(request):
-    template = loader.get_template('main/report.html')
+    template = loader.get_template("main/report.html")
     l = Account.objects.all()
-    context = {'users': [i.serializer_2() for i in l]}
+    context = {"users": [i.serializer_2() for i in l]}
     return HttpResponse(template.render(context, request))
     pass
 
 
 def all_expenses(request):
-    template = loader.get_template('main/expenses.html')
+    template = loader.get_template("main/expenses.html")
     acc = Account.get_account_by_user(request.user.id)
-    context = {'payed_payer': Expense.get_payed_payer_expenses(acc),
-               'payed_debtor': Expense.get_payed_debtor_expenses(acc),
-               'not_payed': Expense.get_not_payed_expenses(acc),
-               'friend_not_payed': Expense.get_friend_not_payed_expenses(acc),
-               'with_url': True}
+    context = {
+        "payed_payer": Expense.get_payed_payer_expenses(acc),
+        "payed_debtor": Expense.get_payed_debtor_expenses(acc),
+        "not_payed": Expense.get_not_payed_expenses(acc),
+        "friend_not_payed": Expense.get_friend_not_payed_expenses(acc),
+        "with_url": True
+    }
     return HttpResponse(template.render(context, request))
 
 
 def report_expenses(request, user_id):
-    template = loader.get_template('main/expenses.html')
+    template = loader.get_template("main/expenses.html")
     acc = Account.get_account_by_user(user_id)
-    context = {'payed_payer': Expense.get_payed_payer_expenses(acc),
-               'payed_debtor':Expense.get_payed_debtor_expenses(acc),
-               'not_payed': Expense.get_not_payed_expenses(acc),
-               'friend_not_payed': Expense.get_friend_not_payed_expenses(acc),
-               'with_url': False}
+    context = {
+        "payed_payer": Expense.get_payed_payer_expenses(acc),
+        "payed_debtor":Expense.get_payed_debtor_expenses(acc),
+        "not_payed": Expense.get_not_payed_expenses(acc),
+        "friend_not_payed": Expense.get_friend_not_payed_expenses(acc),
+        "with_url": False
+    }
     return HttpResponse(template.render(context, request))
 
 
 def pay(request, cost_id):
     if not request.user.is_authenticated:
-        template = loader.get_template('main/index.html')
+        template = loader.get_template("main/index.html")
         context = {}
         return HttpResponse(template.render(context, request))
     else:
         acc = Account.get_account_by_user(request.user.id)
         err = Expense.pay_expenses(cost_id, acc)
         if err is None:
-            return HttpResponseRedirect(reverse('main:expenses'))
-        return HttpResponseRedirect(reverse('main:expenses'))
+            return HttpResponseRedirect(reverse("main:expenses"))
+        return HttpResponseRedirect(reverse("main:expenses"))
 
 
 def edit_profile(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = EditForm(request.POST)
         if form.is_valid():
-            addr = form.cleaned_data['address']
-            city = form.cleaned_data['city']
-            state = form.cleaned_data['state']
-            country = form.cleaned_data['country']
-            phone_number = form.cleaned_data['phone_number']
+            addr = form.cleaned_data["address"]
+            city = form.cleaned_data["city"]
+            state = form.cleaned_data["state"]
+            country = form.cleaned_data["country"]
+            phone_number = form.cleaned_data["phone_number"]
             my_user = Account.get_account_by_user(request.user.id)
             my_user.phone_number = phone_number
             a = Address(address=addr, city=city, state=state, country=country)
@@ -228,78 +257,93 @@ def edit_profile(request):
             return redirect("main:homepage")
     else:
         form = EditForm()
-    return render(request, 'main/edit_profile.html', {'form': form})
+    return render(request, "main/edit_profile.html", {"form": form})
 
 
 def add_share(request, group_id):
     gp = Group.get_group(group_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ShareForm(request.POST)
         form.edit(my_choices=gp.get_members(gp, True))
         if form.is_valid():
-            name = form.cleaned_data['name']
-            addr = form.cleaned_data['address']
-            city = form.cleaned_data['city']
-            state = form.cleaned_data['state']
-            country = form.cleaned_data['country']
+            name = form.cleaned_data["name"]
+            addr = form.cleaned_data["address"]
+            city = form.cleaned_data["city"]
+            state = form.cleaned_data["state"]
+            country = form.cleaned_data["country"]
             a = Address(address=addr, city=city, state=state, country=country)
             a.save()
-            credit = form.cleaned_data['credit']
-            image = form.cleaned_data.get('image')
-            date = form.cleaned_data['date']
-            c_id = form.cleaned_data['creditor']
-            share_type = form.cleaned_data['share_type']
+            credit = form.cleaned_data["credit"]
+            image = form.cleaned_data.get("image")
+            date = form.cleaned_data["date"]
+            c_id = form.cleaned_data["creditor"]
+            share_type = form.cleaned_data["share_type"]
             print(c_id)
-            share = Share(name=name, date=date, address=a, image=image, credit=credit, group_id=group_id,
-                          creditor=Account.get_account_by_user(c_id), share_type= share_type)
+            share = Share(
+                name=name,
+                date=date,
+                address=a,
+                image=image,
+                credit=credit,
+                group_id=group_id,
+                creditor=Account.get_account_by_user(c_id),
+                share_type= share_type
+            )
             share.save()
-            return HttpResponseRedirect(reverse('main:add_share_member', args=(group_id, share.pk)))
+            return HttpResponseRedirect(
+                reverse("main:add_share_member", args=(group_id, share.pk))
+            )
     else:
         form = ShareForm()
         form.edit(my_choices=gp.get_members(gp, True))
-    return render(request, 'main/add_share.html', {
-        'form': form,
-        'group_id': group_id
-    })
+    return render(request, "main/add_share.html", {"form": form, "group_id": group_id})
 
 
 def add_share_member(request, group_id, share_id):
     share_type = Share.get_share_by_id(share_id).share_type
-    if request.method == 'POST':
-        acc_id = request.POST['accounts']
+    if request.method == "POST":
+        acc_id = request.POST["accounts"]
         if share_type == 1:
-            percent = request.POST['percent']
+            percent = request.POST["percent"]
         elif share_type == 2:
-            amount_of_money = request.POST['amount']
+            amount_of_money = request.POST["amount"]
         else:
             percent = 0
             amount_of_money = 0
         print("-------------------------")
         print(percent)
         print(amount_of_money)
-        Share.add_shares(share_id, Account.get_account_by_user(acc_id), percent, amount_of_money)
+        Share.add_shares(
+            share_id, Account.get_account_by_user(acc_id), percent, amount_of_money
+        )
     gp = Group.get_group(group_id)
-    return render(request, 'main/share_member.html',
-                  {'users': gp.get_members(gp),
-                   'group_id': group_id,
-                   'share_id': share_id,
-                   'share_type': share_type
-                   })
+    return render(
+        request,
+        "main/share_member.html",
+        {
+            "users": gp.get_members(gp),
+            "group_id": group_id,
+            "share_id": share_id,
+            "share_type": share_type,
+        },
+    )
 
 
 def end_share_member(request, group_id, share_id):
     share_type = Share.get_share_by_id(share_id).share_type
-    if request.method == 'POST':
-        acc_id = request.POST['accounts']
+    if request.method == "POST":
+        acc_id = request.POST["accounts"]
         if share_type == 1:
-            percent = request.POST['percent']
+            percent = request.POST["percent"]
             amount_of_money = 0
         elif share_type == 2:
-            amount_of_money = request.POST['amount']
+            amount_of_money = request.POST["amount"]
             percent = 0
         else:
             percent = 0
             amount_of_money = 0
-        Share.add_shares(share_id, Account.get_account_by_user(acc_id), percent, amount_of_money)
+        Share.add_shares(
+            share_id, Account.get_account_by_user(acc_id), percent, amount_of_money
+        )
     Share.get_share_by_id(share_id).build_expenses()
-    return HttpResponseRedirect(reverse('main:show_group', args=(group_id,)))
+    return HttpResponseRedirect(reverse("main:show_group", args=(group_id,)))
