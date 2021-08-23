0e16ac21801a9737f2df27607f0fb43c606dc86f
dahlia davoudi <45297749+dahlia-davoudi@users.noreply.github.com>
2021-08-23 05:01:40 +0430

    Update email_service.py

--- SAD_PROJECT/utils/email_service.py
+++ SAD_PROJECT/utils/email_service.py
@@ -3,8 +3,10 @@ from django.core.mail import send_mail
 
 
 def send_email():
-    subject = 'Thank you for registering to our site'
-    message = ' it  means a world to us '
+    subject = "Thank you for registering to our site"
+    message = " it  means a world to us "
     email_from = settings.EMAIL_HOST_USER
-    recipient_list = ['erfanfaravani@gmail.com', ]
+    recipient_list = [
+        "erfanfaravani@gmail.com",
+    ]
     send_mail(subject, message, email_from, recipient_list)
